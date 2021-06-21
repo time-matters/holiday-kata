67648a62611bdbfc9043f741a925dcf13880e9f3
Huiwen Xu <hxu31@dxc.com>
2021-06-21 20:27:10 +0200

    Initialized a Spring Boot project

---
+++ src/main/java/com/timematters/holidaykata/HolidayKataApplication.java
@@ -0,0 +1,13 @@
+package com.timematters.holidaykata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timematters/holidaykata/HolidayKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timematters.holidaykata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
